0521c49a28cba5a052527961f5a7b80ea39b7e32
dafyddllyr <dafyddllyr@gmail.com>
2019-06-18 20:32:59 +0100

    Basic conversion functionality complete:

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      rates: null,
+      base: null,
+      baseInput: 0,
+      selectedCurrency: null
+    },
+    computed: {
+      converted: function() {
+        const result = this.baseInput * this.rates[this.selectedCurrency];
+        return result.toFixed(2);
+      }
+    },
+    mounted() {
+      this.fetchCurrencyData()
+    },
+    methods: {
+      fetchCurrencyData: function() {
+        const request = fetch('https://api.exchangeratesapi.io/latest')
+        .then(response => response.json())
+        .then(data => { this.rates = data.rates; this.base = data.base });
+      }
+    }
+  })
+})
